9f09d41e62d7af79fd6769e3e9665d4a9a2de106
Gyanendra Shukla <srirajshukla@gmail.com>
2021-07-04 09:37:23 +0530

    until hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,102 @@
-import React from 'react';
-import pokemon from './pokemon.json';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import pokemon from "./pokemon.json";
+import "./App.css";
+
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select!</button>
+    </td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+  onSelect: PropTypes.func,
+};
+
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((key) => (
+        <tr key={key}>
+          <td>{key}</td>
+          <td>{base[key]}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  bases: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
 
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState(null);
   return (
     <div
-    style={{
-      margin:"auto",
-      width:800,
-      paddingTop:"1rem",
-    }} 
-     >
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
       <h1 className="title">Pokemon Search</h1>
-    <table width="100%">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Bulbasaur</td>
-          <td>Grass, Poison</td>
-        </tr>
-      </tbody>
-      
-    </table>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          gridColumnGap: "1rem",
+        }}
+      >
+        <div>
+        <input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon
+              .filter((pokemon) =>
+                pokemon.name.english.toLowerCase().includes(filter)
+              )
+              .slice(0, 20)
+              .map((pokemon) => (
+                <PokemonRow
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                  onSelect={(pokemon) => selectedItemSet(pokemon)}
+                />
+              ))}
+          </tbody>
+        </table>
+        </div>
+      {selectedItem && <PokemonInfo {... selectedItem}/>}
+      </div>
     </div>
   );
 }
